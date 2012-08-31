3897288ad5493cc2c253ef8a39c7354dcae193fe
Bronson Quick <bronson@sennza.com.au>
2012-08-31 14:20:53 +1000

    Removes Github banner to fix #8

--- themes/wpaustralia/header.php
+++ themes/wpaustralia/header.php
@@ -13,7 +13,7 @@
 	</head>
 
 	<body <?php body_class(); ?> id="bp-default">
-	<a href="https://github.com/BronsonQuick/wpaustralia"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>
+
 		<?php do_action( 'bp_before_header' ); ?>
 
 		<div id="header">
