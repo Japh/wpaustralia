647c25a67292b3fb5644d2cad3e84f4df661da12
Bronson Quick <bronson@sennza.com.au>
2012-09-05 20:22:56 +1000

    Fixes #15

--- themes/wpaustralia/header.php
+++ themes/wpaustralia/header.php
@@ -4,7 +4,7 @@
 	<head profile="http://gmpg.org/xfn/11">
 		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 		<?php if ( current_theme_supports( 'bp-default-responsive' ) ) : ?><meta name="viewport" content="width=device-width, initial-scale=1.0" /><?php endif; ?>
-		<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+		<title><?php wp_title( '' ); ?></title>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 		<?php do_action( 'bp_head' ); ?>
