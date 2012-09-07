fc1a17ff71892512b4898064d9b518705f98fd24
Japh <japhie@gmail.com>
2012-09-07 11:39:43 +1000

    Fixed up docblock formatting, some code formatting standards, and added a haiku

--- plugins/bp-registration-honeypot/bp-registration-honeypot.php
+++ plugins/bp-registration-honeypot/bp-registration-honeypot.php
@@ -1,20 +1,18 @@
 <?php
 /**
-.* Plugin Name: BuddyPress Registration Honeypot
-.* Plugin URI: http://www.wpaustralia.org
-.* Description: This BuddyPress plugin adds an input field on the registration that is hidden for normal users and will be filled out by bots. If the field contains data then the registration will fail. If it's empty then the registration will continue. This plugin has no options because options are overrated!
-.* Version: 0.2
-.* Author: Bronson Quick
-.* Author URI: http://www.sennza.com.au/
-.* License: GPL2
-.* @package WordPress
-.* @since 0.2
+ * Plugin Name: BuddyPress Registration Honeypot
+ * Plugin URI: http://www.wpaustralia.org
+ * Description: This BuddyPress plugin adds an input field on the registration that is hidden for normal users and will be filled out by bots. If the field contains data then the registration will fail. If it's empty then the registration will continue. This plugin has no options because options are overrated!
+ * Version: 0.2
+ * Author: Bronson Quick
+ * Author URI: http://www.sennza.com.au/
+ * License: GPL2
  */
 
 /**
  * Constant for the plugins version number
+ *
  * @since 0.2
- * @constant BUDDYPRESS_HONEYPOT_REGISTRATION_VERSION The current plugin version number
  */
 define( 'BUDDYPRESS_HONEYPOT_REGISTRATION_VERSION', '0.2' );
 
@@ -22,12 +20,14 @@ $sennza_buddypress_registration_honeypot = new Sennza_BuddyPress_Registration_Ho
 
 /**
  * Where all the awesome happens of course :)
+ *
  * @since 0.2
  */
 class Sennza_BuddyPress_Registration_HoneyPot {
 
 	/**
 	 * Setup all the magic for our honeypot
+	 *
 	 * @since 0.2
 	 */
 	public function __construct(){
@@ -38,25 +38,27 @@ class Sennza_BuddyPress_Registration_HoneyPot {
 
 	/**
 	 * This function adds the honeypot field after the account details in BuddyPress
+	 *
 	 * @since 0.2
 	 */
 	public function sennza_buddypress_registration_honeypot() {
 		?>
-	<?php //Lets wrap this and hide it with CSS ?>
-	<div id="basic-details-honeypot-section" class="honeypot_container register-section">
-		<?php //Lets give it a dummy label that spam bots will think is real ?>
-		<label for="signup_honeypot"><?php _e( 'Phone' ); ?></label>
-		<?php //I can't see us needing to output errors but I'll add an action here in case accessibility issues come up! ?>
-		<?php do_action( 'sennza_buddypress_registration_honeypot_errors' ); ?>
-		<input type="text" name="signup_honeypot" id="signup_honeypot" value="" />
-		<?php $sennza_honeypot_warning = apply_filters( 'sennza_honeypot_warning', 'This field is for validation purposes and should be left blank.' );?>
-		<p><?php esc_html_e( $sennza_honeypot_warning ); ?></p>
-	</div>
-	<?php
+		<?php //Lets wrap this and hide it with CSS ?>
+		<div id="basic-details-honeypot-section" class="honeypot_container register-section">
+			<?php //Lets give it a dummy label that spam bots will think is real ?>
+			<label for="signup_honeypot"><?php _e( 'Phone' ); ?></label>
+			<?php //I can't see us needing to output errors but I'll add an action here in case accessibility issues come up! ?>
+			<?php do_action( 'sennza_buddypress_registration_honeypot_errors' ); ?>
+			<input type="text" name="signup_honeypot" id="signup_honeypot" value="" />
+			<?php $sennza_honeypot_warning = apply_filters( 'sennza_honeypot_warning', 'This field is for validation purposes and should be left blank.' );?>
+			<p><?php esc_html_e( $sennza_honeypot_warning ); ?></p>
+		</div>
+		<?php
 	}
 
-	/*
+	/**
 	 * We need to add a signup error to the $bp global so that the registration process fails
+	 *
 	 * @since 0.2
 	 * @global array $bp
 	 */
@@ -66,20 +68,21 @@ class Sennza_BuddyPress_Registration_HoneyPot {
 			$bp->signup->errors['signup_honeypot'] = apply_filters( 'sennza_honeypot_error', __( 'Looks like you are a spammer' ) );
 	}
 
-	/*
-	 * We need to enqueue the CSS for this plugin.
+	/**
+	 * Enqueues the CSS for this plugin.
+	 *
 	 * @since 0.2
 	 */
 	public function sennza_buddypress_registration_honeypot_css() {
 		/* Props to Boone for picking up on me enqueue the CSS on all the pages :) */
-		if ( bp_is_register_page() ):
+		if ( bp_is_register_page() ) {
 			wp_register_style(
-			'honeypot-css',
-			plugins_url( '/css/bp-honeypot.css',__FILE__ ),
-			false,
-			BUDDYPRESS_HONEYPOT_REGISTRATION_VERSION
+				'honeypot-css',
+				plugins_url( '/css/bp-honeypot.css', __FILE__ ),
+				array(),
+				BUDDYPRESS_HONEYPOT_REGISTRATION_VERSION
 			);
 			wp_enqueue_style( 'honeypot-css' );
-		endif;
+		}
 	}
 }
